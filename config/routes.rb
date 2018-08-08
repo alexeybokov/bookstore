8dbf7e2337ce940119da59b1f773da0c570a79e6
alexeybokov <lexabokov18@gmail.com>
2018-08-08 12:58:49 +0300

    update code

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   resources :order_items, only: [:index, :update, :destroy]
   resources :orders, only: [:index, :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'books#index'
-  get 'users/profile'
-  get 'persons/profile', as: 'user_root'
+  root 'dashboard#index'
 end
