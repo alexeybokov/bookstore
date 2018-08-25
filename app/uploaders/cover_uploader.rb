3e3a2ce641e1bead64df7ec687ca5a68b90ab7fe
alexeybokov <lexabokov18@gmail.com>
2018-08-25 23:52:48 +0300

    add cover and aws

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -33,11 +33,21 @@ class CoverUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [500, 500]
   end
 
+  version :small do
+    process resize_to_fit: [150, 150]
+    process quality: 100
+  end
+
+  version :medium do
+    process resize_to_fit: [300, 300]
+    process quality: 100
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
