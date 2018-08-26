6c8ade8121391bc2815dd43e10280beca5c3950e
alexeybokov <lexabokov18@gmail.com>
2018-08-26 12:29:53 +0300

    wip

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+    aws_access_key_id:     'AKIAJJWANRUNNWMKWC5A',
+    aws_secret_access_key:  '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',
     region:                 'us-east-1'
   }
 
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
   end
 
   config.cache_dir     = "#{Rails.root}/tmp/uploads"
-  config.fog_directory = ENV ['S3_BUCKET']
+  config.fog_directory = 'bookstore-rails'
 end
 
 module CarrierWave
