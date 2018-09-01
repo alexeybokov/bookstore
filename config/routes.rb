ccbebcf428b537dc3aa0609433774fa6b483492f
alexeybokov <lexabokov18@gmail.com>
2018-09-01 19:58:06 +0300

    update view fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
   resources :books, path: "books" do
     collection do
       get :newest
@@ -8,9 +11,8 @@ Rails.application.routes.draw do
       get :price_desc
     end
   end
-  resources :books, only: [:index, :show]
   resources :categories, only: [:index, :show]
-  resources :order_items, only: [:index, :update, :destroy]
+  resources :order_items, only: [:index, :create, :update, :destroy]
   resources :orders, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#index'
