c5b50f79f09a0d17c0c0b82163391a5bcbe66b73
alexeybokov <lexabokov18@gmail.com>
2018-09-03 23:13:15 +0300

    update db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
 
-  # Category.create(title: 'Mobile')
-  Category.create!(title: 'Development')
-  # Category.create(title: 'Photo')
-  # Category.create(title: 'Web design')
-  # Category.create(title: 'Web development')
+Order.destroy_all
+Author.destroy_all
+Book.destroy_all
+Category.destroy_all
+User.destroy_all
 
-25.times do
-  Author.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    biography: Faker::Lorem.paragraph(3, true)
+
+User.create(email: 'admin@gmail.com',
+            password: 'admin')
+
+
+5.times do |index|
+  category = Category.create!(title: Faker::Book.title)
+
+
+  15.times do
+    book = Book.create!(
+      title: Faker::Book.title,
+      description: Faker::Lorem.sentence(50, true).chop,
+      price: Faker::Commerce.price,
+      in_stock: Faker::Commerce.price(range = 1..10),
+      author: Author.first,
+      category: category
     )
-end
 
-25.times do
-  Book.create!(
-    title: Faker::Book.title,
-    description: Faker::Lorem.sentence(50, true).chop,
-    price: Faker::Commerce.price,
-    in_stock: Faker::Commerce.price(range = 1..10),
-    author: Author.first,
-    category: Category.last
+    Author.create!(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      biography: Faker::Lorem.paragraph(3, true),
+      books: [book]
     )
+  end
 end
