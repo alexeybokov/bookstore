f0357589f63a305ce1fde81745959cd0a2d5b6f1
alexeybokov <lexabokov18@gmail.com>
2019-09-11 18:40:20 +0300

    some configs

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,17 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:               'AWS',
-    aws_access_key_id:     '****************************',
-    aws_secret_access_key:  '*********************************',
-    region:                 'us-east-1'
-  }
+  # config.fog_credentials = {
+  #   provider:               'AWS',
+  #   aws_access_key_id:     '****************************',
+  #   aws_secret_access_key:  '*********************************',
+  #   region:                 'us-east-1'
+  # }
 
   if Rails.env.test? || Rails.env.cucumber?
     config.storage           = :file
     config.enable_processing = false
     config.root              = "#{Rails.root}/tmp"
   else
-    config.storage           = :fog
+    config.storage           = :file
   end
 
   config.cache_dir     = "#{Rails.root}/tmp/uploads"
